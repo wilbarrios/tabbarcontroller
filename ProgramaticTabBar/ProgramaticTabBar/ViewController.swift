ac46198254e07eec2756207dc9080ab61ddb2895
Wilmer Barrios <wilmerbt@yahoo.com>
2021-04-13 19:13:47 -0500

    Project initalization

---
+++ ProgramaticTabBar/ProgramaticTabBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgramaticTabBar
+//
+//  Created by Wilmer Barrios on 13/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
