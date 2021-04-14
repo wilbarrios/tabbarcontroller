3c5f9b84504f9575c4c519cac82b405b4e835b6c
Wilmer Barrios <wilmerbt@yahoo.com>
2021-04-13 19:16:58 -0500

    `RootTabBarController` setup

---
+++ ProgramaticTabBar/ProgramaticTabBar/RootTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  RootTabBarController.swift
+//  ProgramaticTabBar
+//
+//  Created by Wilmer Barrios on 13/04/21.
+//
+
+import Foundation
+import UIKit
+
+class RootTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.systemOrange
+    }
+}
