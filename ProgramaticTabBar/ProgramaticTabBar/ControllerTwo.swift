f1d20295c3223dc0ed6cc1a24bfc3903a25699b4
Wilmer Barrios <wilmerbt@yahoo.com>
2021-04-13 19:50:41 -0500

    `ControllerTwo` button added

--- ProgramaticTabBar/ProgramaticTabBar/ControllerTwo.swift
+++ ProgramaticTabBar/ProgramaticTabBar/ControllerTwo.swift
@@ -9,12 +9,36 @@ import Foundation
 import UIKit
 
 class ControllerTwo: UIViewController {
+    var onButtonClicked: (() -> Void)?
+    
+    private let button: UIButton = {
+        let b = UIButton()
+        b.backgroundColor = UIColor.white
+        b.setTitle("To Controller One", for: .normal)
+        b.setTitleColor(.systemBlue, for: .normal)
+        b.addTarget(self, action: #selector(buttonHandler), for: .touchUpInside)
+        b.translatesAutoresizingMaskIntoConstraints = false
+        return b
+    }()
+    
+    @objc
+    private func buttonHandler() {
+        onButtonClicked?()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setupLayout()
+        setupLayout()
     }
     
     private func setupLayout() {
-        self.view.backgroundColor = UIColor.blue
+        view.backgroundColor = UIColor.blue
+        
+        view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
     }
 }
